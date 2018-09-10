cf5b37e0c649a199a3aaaae0fef887b7b4811911
Alkxzv <carlos@lkxz.net>
2018-09-10 20:02:45 +0200

    Initial commit

---
+++ backend/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+type Record struct {
+	Source  string
+	Target  string
+	Amount  int
+	Concept string
+}
+
+var records = []Record{
+	Record{Source: "Employer", Target: "Bank", Amount: 100000, Concept: "Salary"},
+	Record{Source: "Bank", Target: "Amazon", Amount: 2500, Concept: "Book"},
+}
+
+func main() {
+	router := mux.NewRouter()
+	router.HandleFunc("/records", recordsHandler)
+	http.ListenAndServe(":8000", router)
+}
+
+func recordsHandler(w http.ResponseWriter, r *http.Request) {
+	payload, _ := json.Marshal(records)
+	w.Header().Set("Content-Type", "application/json")
+	w.Write([]byte(payload))
+}
